57e03dc3169b753d92ba9f7d00dc4c4dafc96e46
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2025-07-01 06:16:01 +0300

    Update casery.py

--- casery.py
+++ casery.py
@@ -8,16 +8,16 @@ import os
 # bot_token2 = os.getenv("SESSION_STRING")
 
 ## مطورين السورس (Sudo users of the maker bot)
-caes = ["Fox_fv", "zozooryy", "@cyv0we"]  # Add your main developer usernames here
+caes = ["f_o_x_351", "zozooryy", "cyv0we"]  # Add your main developer usernames here
 
 ## يوزر المطور الاساسي (The main developer's username)
-casery = os.getenv("OWNER_USERNAME", "TI_1Too")
+casery = os.getenv("OWNER_USERNAME", "f_o_x_351")
 
 ## ايدي مطور السورس الاساسي (The main developer's user ID)
-caserid = int(os.getenv("OWNER_ID", "6908157965"))
+caserid = int(os.getenv("OWNER_ID", "7669264153"))
 
 ## اسم البوتات بالعربى (Default bot name)
-OWNER = os.getenv("BOT_NAME_AR", "النسور")
+OWNER = os.getenv("BOT_NAME_AR", "fox")
 
 ## اسمك&الميوزك (Music text)
 muusiic = os.getenv("MUSIC_TEXT", "SOURCE Titanx")
@@ -36,8 +36,8 @@ group = os.getenv("SOURCE_GROUP_LINK", "https://t.me/fox68899")
 photosource = os.getenv("SOURCE_PHOTO_URL", "https://envs.sh/ws4.webp")
 
 ## توكن الصانع (Maker Bot Token)
-bot_token = os.getenv("BOT_TOKEN", "8190727627:AAFurLfneMl9KGSx7pdY84pTJVw_mxaYlxE")
+bot_token = os.getenv("BOT_TOKEN", "7704080555:AAEuYnTuELd44t_cdlfyWdcRVxmIhM9J-Rg")
 
 ## جلسه الاشتراك (Helper Account Session String)
-bot_token2 = os.getenv("SESSION_STRING", "BAGHeQMAQFDc6TAIweY7uD5nxSOa2MyJ_nZVzaPAd6VVtId_Ij8SHiwzmgNbboOSvtVXekosc8rBllmxd1_YVD5oPaKiN70UFr-yFIxKbyX5TJ0ZNUbu0RQy4CstT_XhkjNLtuOnKVoU8dL_dqUghscOan2JPJUWJnLAygYs6hjs-ILXSkYDHYF5SfXCN_fe94l7fqjMEZYwNUZ80IYl5b6_uP5HDw8c90P6VC-BDhEky41CoQ8caKPmvBrFRPuTyjkQfxaZkEmA3oje1QpZCU8xe9Efy-tmmY3wX8vl5OOHoQ6YVkvKBAMrgs9EK5VqtQT4vVXuigkR3MjdcdDF7IWXFzmhsAAAAAHJH68ZAA")
+bot_token2 = os.getenv("SESSION_STRING", "BAGHeQMAxnmdUxz0Ohm7kaiip9SBmORIiXLZWW6rmYRGODFFqiwwkyQBIa6E7UqcjHadyz_08JizJBkrUvGr1jxy46ZPZlvykDRNMuu4siErnunhhEJ54KJ0fBBAbCH5z4PXHKI5QAdWIOOxjbgXqQBkkn_Jg3yhBS9GwLDeKbGy6xiOPtLD2LJ-nzvuv2BeMHm3G-jCgrxZ8AUCM8qw0CqmMr3G8H6R9jLfSRLzN1xFVN2TvfsqqCmB5j6He0bg1i9gm2IYQYOd6wuk3AJJiIq_Rb92RmA00B6WqIhuWIwcI7YvIEUOQmrR1c4HO7Cqpmu_Bb8-coZ200x7eh6KqQE5VSO7uQAAAAHJH68ZAA")
 # --- END OF FILE casery.py ---
